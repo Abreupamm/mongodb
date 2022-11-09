515f661c9e5ed45c0624c9ffbaa4126fdf71c1ef
Pamela <abrepamm@gmail.com>
2022-11-09 19:51:59 -0300

    feat: utilizando o operador $set desafio 16

---
+++ challenges/desafio16.js
@@ -0,0 +1,13 @@
+db.produtos.updateMany({ 
+  nome: "Big Mac",
+}, {
+  $set: { ultimaModificacao: Date },
+ });
+
+ db.produtos.find({
+  ultimaModificacao: { $exists: true },
+}, {
+  _id: 0, 
+  nome: 1, 
+  avaliacao: 1,
+});
